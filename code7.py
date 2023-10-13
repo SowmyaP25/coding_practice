b77f791d03cf17102f521c42ce85b4013998fa2e
SowmyaP25 <146048732+SowmyaP25@users.noreply.github.com>
2023-10-13 10:51:33 +0530

    leap year and eucliden distance

---
+++ code7.py
@@ -0,0 +1,5 @@
+year = int(input("Enter the Year: "))
+if ((year%400 == 0)or ((year%4 == 0 ) and (year%100 != 0))):
+    print(year,"is Leap Year")
+else:
+    print(year,"is Not the Leap Year")
---
+++ code8.py
@@ -0,0 +1,6 @@
+x1 = int(input('Enter x1 value: '))
+x2 = int(input('Enter x2 value: '))
+y1 = int(input('Enter y1 value: '))
+y2 = int(input('Enter y2 value: '))
+distance = ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+print('The Euclidean distance between (x1, y1) and (x2, y2):', distance)
